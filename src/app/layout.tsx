e981e4ae4b7325d3aecc48f3531b2b0c9e5e19d1
Kyle Yeung <kyleyeung6@gmail.com>
2025-10-29 03:28:38 +0000

    i said make the get started page first

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
           crossOrigin="anonymous"
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&family=Montserrat:wght@400;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&family=Montserrat:wght@700&display=swap"
           rel="stylesheet"
         />
       </head>
